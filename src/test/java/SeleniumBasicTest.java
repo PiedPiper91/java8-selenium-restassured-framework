1c5efc0949db2d2956b82f01e725244f9717d176
Arya Bhattacharya <arya_bhattacharya@epam.com>
2026-08-12 13:54:42 +0530

    action items

--- src/test/java/SeleniumBasicTest.java
+++ src/test/java/SeleniumBasicTest.java
@@ -3,13 +3,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import utilities.LoggerUtil;
-
-import javax.xml.datatype.Duration;
+import java.time.Duration;
 
 import static constants.FrameworkConstants.DEFAULT_TIMEOUT;
 
@@ -17,25 +17,41 @@ public class SeleniumBasicTest {
 
     WebDriver driver;
 
+    @BeforeMethod
+    public void setup() {
+        if (driver == null) {
+            driver = new ChromeDriver();
+            LoggerUtil.info("Browser launched");
+        }
+    }
+
     @Test
-    void testDateNotNull() throws InterruptedException {
-        driver = new ChromeDriver();
-        LoggerUtil.info("Browser launched");
+    void testOrangeHRMLogin() throws InterruptedException {
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         LoggerUtil.info("Navigated to "+driver.getCurrentUrl());
         LoggerUtil.info("Page Title: "+driver.getTitle());
-        WebElement usernameInput = driver.findElement(By.name("username"));
-        WebElement passwordInput = driver.findElement(By.name("password"));
-        WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit' and contains(@class,'orangehrm-login-button')]"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(DEFAULT_TIMEOUT));
+        WebElement usernameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+        WebElement passwordInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));
+        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit' and contains(@class,'orangehrm-login-button')]")));
         usernameInput.sendKeys("Admin");
         passwordInput.sendKeys("admin123");
         loginButton.click();
         try {
-            driver.wait(2000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        Assert.assertTrue(driver.getCurrentUrl().contains("dashboard"));
+        String currentUrl = driver.getCurrentUrl();
+        LoggerUtil.info("Current URL after login: " + currentUrl);
+        Assert.assertTrue(currentUrl.contains("dashboard"),"Login failed. Expected dashboard URL but found: " + currentUrl);
         driver.quit();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
